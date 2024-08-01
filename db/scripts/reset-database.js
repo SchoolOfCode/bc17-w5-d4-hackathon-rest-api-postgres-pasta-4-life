000b769cbf414f19d957cc023d480534c9d0d34e
Isuu1 <jakub.babelek@gmail.com>
2024-08-01 14:19:58 +0100

    Created basics

--- db/scripts/reset-database.js
+++ db/scripts/reset-database.js
@@ -1,54 +1,50 @@
 import { pool } from "../index.js";
 
-
-
 // >>> MAKE SURE YOU UNDERSTAND THIS FILE AND WHAT IT'S DOING <<<
-// >>> FEEL FREE TO CHANGE IT TO MAKE YOUR OWN RESOURCES (TABLES AND PROPERTIES) - YOU DON'T HAVE TO USE ALBUMS AND ARTISTS <<<
-
-
+// >>> FEEL FREE TO CHANGE IT TO MAKE YOUR OWN RESOURCES (TABLES AND PROPERTIES) - YOU DON'T HAVE TO USE movies AND directors <<<
 
 async function resetDatabase() {
   try {
     // Drop existing tables if they exist
     await pool.query(`
-        DROP TABLE IF EXISTS artists CASCADE;
-        DROP TABLE IF EXISTS albums CASCADE;
+        DROP TABLE IF EXISTS directors CASCADE;
+        DROP TABLE IF EXISTS movies CASCADE;
     `);
 
-    // Create the artists table
+    // Create the directors table
     await pool.query(`
-        CREATE TABLE artists (
+        CREATE TABLE directors (
             id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
             name VARCHAR(255) NOT NULL
         );
     `);
 
-    // Create the albums table with a foreign key to the artists table
+    // Create the movies table with a foreign key to the directors table
     await pool.query(`
-        CREATE TABLE albums (
+        CREATE TABLE movies (
             id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
             title VARCHAR(255) NOT NULL,
             published_date DATE,
-            artist_id INT REFERENCES artists(id)
+            director_id INT REFERENCES directors(id)
         );
     `);
 
-    // Seed the artists table
+    // Seed the directors table
     await pool.query(`
-        INSERT INTO artists (name)
+        INSERT INTO directors (name)
         VALUES 
-            ('Dua Lipa'),
-            ('Jay-Z');
+            ('Steven Spielberg'),
+            ('Martin Scorsese');
     `);
 
-    // Seed the albums table
+    // Seed the movies table
     await pool.query(`
-        INSERT INTO albums (title, published_date, artist_id)
+        INSERT INTO movies (title, published_date, director_id)
         VALUES 
-            ('Dua Lipa', '2017-06-02', 1),
-            ('Future Nostalgia', '2020-03-27', 1),
-            ('Reasonable Doubt', '1996-06-25', 2),
-            ('The Blueprint', '2001-09-11', 2);
+            ('Steven Spielberg', '2017-06-02', 1),
+            ('Martin Scorsese', '2020-03-27', 1),
+            ('Christopher Nolan', '1996-06-25', 2),
+            ('Alfred Hitchcock', '2001-09-11', 2);
     `);
 
     console.log("Database reset successful");
