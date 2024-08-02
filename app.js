31427b7c3dbfa7d6fbd013d1f1862f88ec3f9654
Isuu1 <jakub.babelek@gmail.com>
2024-08-02 11:43:58 +0100

    Cleaned a code

--- app.js
+++ app.js
@@ -29,33 +29,10 @@ app.use(express.json()); // express.json() middleware is used to parse incoming 
 // Resource One Route Handlers
 
 // Endpoint to retrieve all directors information for every director
-app.get("/directors/", async function (req, res) {
-  // await function to retrieve all director information for every director
-  try {
-    // Get data from helper function
-    const data = await getDirectors();
-    // Send back response if correct
-    res.status(200).json({ success: true, payload: data });
-  } catch (error) {
-    console.error("Problem getting data");
-    // Send back response if incorrect
-    res.status(400).json({ success: false, payload: null });
-  }
-});
+app.get("/directors/", async function (req, res) {});
 
 // Endpoint to retrieve a <resource_one> by id
-app.get("/directors/:id", async function (req, res) {
-  try {
-    // Get current ID from request params
-    const id = req.params.id;
-    // Get data from helper function
-    const data = await getDirector(id);
-    res.status(200).json({ success: true, payload: data });
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({ success: false, payload: null });
-  }
-});
+app.get("/directors/:id", async function (req, res) {});
 
 // Endpoint to create a new <resource_one>
 app.post("/resourceone/", async function (req, res) {});
@@ -90,3 +67,26 @@ app.delete("/resourcetwo/:id", async function (req, res) {});
 app.listen(PORT, function () {
   console.log(`Server listening on port ${PORT}`);
 });
+
+// await function to retrieve all director information for every director
+// try {
+//   // Get data from helper function
+//   const data = await getDirectors();
+//   // Send back response if correct
+//   res.status(200).json({ success: true, payload: data });
+// } catch (error) {
+//   console.error("Problem getting data");
+//   // Send back response if incorrect
+//   res.status(400).json({ success: false, payload: null });
+// }
+
+// try {
+//   // Get current ID from request params
+//   const id = req.params.id;
+//   // Get data from helper function
+//   const data = await getDirector(id);
+//   res.status(200).json({ success: true, payload: data });
+// } catch (error) {
+//   console.log(error);
+//   res.status(400).json({ success: false, payload: null });
+// }
